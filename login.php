e84519c015914138a74f0511a2a8cd9082c947a5
Jarwonozt <67833034+jarwonozt@users.noreply.github.com>
2023-09-12 23:50:59 +0700

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 <body>
   <div class="wrapper">
     <section class="form login">
-      <header>Realtime Chat App</header>
+      <header>Realtime Chat App use PHP JS</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
